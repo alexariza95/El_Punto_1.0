80f71a6ccffa4af675e48ca70bb1634ac3bcb4a7
p42argea <alex1995ariza@gmail.com>
2018-09-26 18:00:45 +0200

    Añadido nuevo archivo

---
+++ codigo_simple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y;
+	printf("Introduzca la X\n");
+	scanf("%d",&x);
+	printf("Introduzca, ahora, la coordenada Y\n");
+	scanf("%d",&y);
+
+	printf("La suma de las coordenadas es: %d\n", x+y);
+}
